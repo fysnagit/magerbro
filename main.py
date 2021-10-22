0c043b47f0f459bbe2c40f9daa2f7bbf847c162a
fysnagit <92987747+fysnagit@users.noreply.github.com>
2021-10-22 20:46:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,6 @@ def index():
   
   name_file = rs_generator()
   test.save(f'{name_file}.docx')
-  convert(f"{name_file}.docx")
   return send_file(f"{name_file}.docx",as_attachment=True)
  return render_template(f'index.html')
 
